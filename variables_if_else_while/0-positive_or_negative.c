3f1bf5d96b715b0d61904018049787045baebb96
Abdulrahman <dhomfff@gmail.com>
2026-02-27 01:07:02 +0300

    adding first task

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+	printf("%i  is positive", n);
+	}
+	else
+	{
+	if (n == 0)
+	{
+	printf("%i  is negative", n);
+	}
+	else
+	{
+	if (n == 0)
+	printf("%i  is zero", n);
+	}
+	}
+	return (0);
+}
