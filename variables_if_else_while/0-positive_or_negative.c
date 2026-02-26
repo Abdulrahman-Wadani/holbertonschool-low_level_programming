6242fd37f4c7abf8c8101917e11509004b93e9e4
Abdulrahman <dhomfff@gmail.com>
2026-02-27 01:13:10 +0300

    adding first task(try4)

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -17,17 +18,13 @@ int main(void)
 	{
 	printf("%i  is positive\n", n);
 	}
-	else
-	{
-	if (n == 0)
+	else if (n < 0)
 	{
 	printf("%i  is negative\n", n);
 	}
-	else
-	{
-	if (n == 0)
+	else if (n == 0)
 	printf("%i  is zero\n", n);
-	}
-	}
+	
+	
 	return (0);
 }
