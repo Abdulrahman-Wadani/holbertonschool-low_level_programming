3f9ddd0417a6cc342988676848c8d3f9cd8d685f
Abdulrahman <dhomfff@gmail.com>
2026-02-27 23:59:07 +0300

    adding the second task(try2)

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 	j = n % 10;
 	/* your code goes there */
 	if (j > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, j);
+		printf("Last digit of %d is %d and is greater than 5\n", n, j);
 	if (j == 0)
-                printf("Last digit of %d is %d and is 0", n, j);
+                printf("Last digit of %d is %d and is 0\n", n, j);
 	if (j < 6 && j > 0)
-                printf("Last digit of %d is %d and is less than 6 and not 0", n, j);
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j);
 	return (0);
 }
