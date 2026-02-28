3fd854cbb18f17817a71be62bfb6c7af97ef978a
Abdulrahman <dhomfff@gmail.com>
2026-02-28 23:19:51 +0300

    adding 9th task

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * eturn: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+		for (j = 0; j < 60; j++)
+		{
+			if(i < 10)
+				_putchar('0');
+			_putchar(i + '0');
+			_putchar(i + '0');
+			_putchar(':');
+			if (j < 10)
+				_putchar('0');
+			_putchar(j + '0');	
+		}
+}
