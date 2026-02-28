8227684fbc71add9d61b55553c6ebf71420d374d
Abdulrahman <dhomfff@gmail.com>
2026-02-28 22:58:11 +0300

    adding 8th task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,15 @@
 
 /**
  * print_last_digit - Entry point
- * @n: 
+ * @n: number 
  *
  * Return: Always 0 (Success)
  */
 int print_last_digit(int n)
 {
 	n = n % 10;
-
+	if (n < 0)
+		n *= -1
 	_putchar(n + '0');
 	return (n);
 }
