0e2ba696d6aae099bfd322d5515f045c7d3d41b4
Abdulrahman <dhomfff@gmail.com>
2026-02-28 22:44:10 +0300

    adding 7th task

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: number
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+	return (0);
+}
