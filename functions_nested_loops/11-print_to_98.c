0019ebe7f13b6c8aa4cb22f2ed0d9eb9a3e29bd4
Abdulrahman <dhomfff@gmail.com>
2026-03-01 00:02:51 +0300

    adding 11th task7

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,19 @@
  */
 void print_to_98(int n)
 {
-        if (n <= 98)
-        {
-                for (; n < 98; n++)
-                {
-                        printf("%d, ", n);
-                }
-        }
-        else
-        {
-                for (; n > 98; n--)
-                {
-                        printf("%d, ", n);
-                }
-        }
-        
-        /* طباعة الرقم الأخير دائماً متبوعاً بسطر جديد */
-        printf("98\n");
+	if (n <= 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for (; n > 98; n--)
+		}
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
 }
