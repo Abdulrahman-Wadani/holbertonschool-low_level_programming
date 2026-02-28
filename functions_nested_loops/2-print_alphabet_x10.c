aab0581438092f7bd54030f988a39e7cf94300f1
Abdulrahman <dhomfff@gmail.com>
2026-02-28 21:59:15 +0300

    adding Third task(3)

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		int j;
 		for (j = 97; j <123; j++)
-			_putchar(i);
+			_putchar(j);
 		_putchar('\n');
 	}
-	return (0);
 }
