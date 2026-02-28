7e32c4a3fd88517ce2a7bb860d2862d746435e5d
Abdulrahman <dhomfff@gmail.com>
2026-02-28 21:41:23 +0300

    adding Second task(2)

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -10,18 +10,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-/**
- * print_alphabet - a to z
- *
- * Return: 0
- */
-int print_alphabet(void)
-{
-	int i;
-
-	for (i = 97; i < 123; i++)
-		_putchar(i);
-	_putchar('\n');
-	return(0);
-}
