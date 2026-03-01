ecaec6d9717014e812b182eca2ee319673a690aa
Abdulrahman <dhomfff@gmail.com>
2026-03-01 06:51:10 +0300

    adding 7th task

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: number
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (i <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
