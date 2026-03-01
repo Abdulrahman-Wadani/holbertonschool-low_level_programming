3bceb2e0907c01f422073c0dfb55dbbf3f69e960
Abdulrahman <dhomfff@gmail.com>
2026-03-01 07:24:39 +0300

    adding 10th task

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d ", n);
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i == 100)
+			printf("Buzz\n");
+		else
+			printf("Buzz ");
+	return (0);
+}
