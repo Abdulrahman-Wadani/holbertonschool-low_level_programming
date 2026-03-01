1243521bf975b449384ef3ba303963041b316122
Abdulrahman <dhomfff@gmail.com>
2026-03-01 09:34:45 +0300

    adding 11th task10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
-	int c = size - 1;
+	int c;
 
 	if (size <= 0)
 		_putchar('\n');
@@ -19,6 +19,7 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
+			c = size - i;
 			for (; c > 0; c--)
 				_putchar(' ');
 			for (j = 0; j < i; j++)
