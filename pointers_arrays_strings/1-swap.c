4e78151d7da64ac360794ef52cad80f54b5a20fa
Abdulrahman <dhomfff@gmail.com>
2026-03-02 01:03:58 +0300

    adding Second task

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a:
+ * @b:
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *b;
+
+	*a = *b;
+	*b = *c
+
+}
