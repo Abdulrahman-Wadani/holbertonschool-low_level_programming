8fd08505724814ab2c66a817910544e3d21de4c1
Abdulrahman <dhomfff@gmail.com>
2026-03-02 02:18:33 +0300

    adding 5ht task

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * @s: char
+ *
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	char c;
+	int counter = 0;
+
+	for (c = s[0]; c != '\0'; c = s[counter])
+		counter++;
+	for (; counter > -1; counter--)
+		_putchar(s[counter]);
+		
+
+}
