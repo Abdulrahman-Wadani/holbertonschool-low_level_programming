85c46481bd83c5d552c984fb63d7ef0ffdcfdaaa
Abdulrahman <dhomfff@gmail.com>
2026-03-01 07:01:52 +0300

    adding 8th task

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: number
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\');
+			_putchar('\n');
+			for (j = 0; j < i + 1; j++)
+				_putchar(' ');
+		}
+	}
+}
