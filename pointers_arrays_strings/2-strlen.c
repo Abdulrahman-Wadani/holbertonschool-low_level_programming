1edfc8125dfaac2be59272dfb2c6f7070834088d
Abdulrahman <dhomfff@gmail.com>
2026-03-02 02:02:49 +0300

    adding Third task

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: char
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	char i;
+	int counter;
+
+	for (i = s[0]; i != '\0' i++)
+	{
+		i = s[counter + 1];
+		counter++;
+	}
+	return (counter);
+}
