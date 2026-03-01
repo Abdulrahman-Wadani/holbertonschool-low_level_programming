366ebcbff862315bc8c93b4d4faa764501f8e470
Abdulrahman <dhomfff@gmail.com>
2026-03-01 06:23:50 +0300

    adding 6th task6

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,20 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int c = 0;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar((j / 10) + '0');
-			if (j > 10)
+			_putchar(c + '0');
+			if (j < 10)
+				c++;
+			if (j > 9)
+			{
+				c = 1;
 				_putchar((j % 10) + '0');
-		}
+			}
 		_putchar('\n');
 	}
 }
