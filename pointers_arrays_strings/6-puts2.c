7029da98f06d2cf75b7e598efbac69f7bb622d62
Abdulrahman <dhomfff@gmail.com>
2026-03-04 02:50:48 +0300

    adding 7ht task

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int c = 0;
+	char ch;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+
+	while (i < c)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+}
