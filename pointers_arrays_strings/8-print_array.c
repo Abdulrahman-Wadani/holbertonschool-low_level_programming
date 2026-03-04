362ada02031116dc887dd2d710d6564fc68f0159
Abdulrahman <dhomfff@gmail.com>
2026-03-04 05:02:57 +0300

    adding 9th task

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Entry point
+ * @a: array
+ * @n: size
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	while (i < n)
+	{
+		if (i == n)
+			printf("%d\n", a[i++]);
+		else
+			printf("%d, ", a[i++]);
+	}
+}
