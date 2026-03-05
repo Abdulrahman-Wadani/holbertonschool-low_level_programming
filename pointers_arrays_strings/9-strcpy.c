18b216f6153d32b745f8579c3fb7531d40ef225c
Abdulrahman <dhomfff@gmail.com>
2026-03-05 09:27:56 +0300

    adding 10th task

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - Entry point
+ * @dest: dest
+ * @src: src
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *p;
+	int c = 0;
+	int c2 = 0
+
+	while (src[c] != '\0')
+		c++;
+
+	while (c2 <= c)
+		dest[c2++] = src[c];
+
+	p = &dest;
+
+	return(p);
+}
