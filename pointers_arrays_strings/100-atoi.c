4edfec09d13a27b805cc2719ed5f7e6cd3dc273d
Abdulrahman <dhomfff@gmail.com>
2026-03-05 11:37:50 +0300

    adding 11th task9

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,13 @@
  */
 int _atoi(char *s)
 {
-	int len_of_s = 0; int i = 0; int number_Sign = 1; int my_bool = 0;
-	int bool_startOfNumber = 0; unsigned int returned;
+	int i = 0;
+	int number_Sign = 1;
+	int my_bool = 0;
+	int bool_startOfNumber = 0;
+	unsigned int my_return;
 
-	while (s[len_of_s] != '\0')
-		len_of_s++;
-	while (i <= len_of_s)
+	while (s[i] != '\0')
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			my_bool = 1;
@@ -24,27 +25,25 @@ int _atoi(char *s)
 	i = 0;
 	if (my_bool == 1)
 	{
-		while (i <= len_of_s)
+		while (s[i] != '\0')
 		{
 			if (bool_startOfNumber == 1 && !(s[i] >= '0' && s[i] <= '9'))
 			{
 				if (number_Sign == 1)
-					return (returned);
+					return (my_return);
 				else
-					return (-returned);
+					return (-my_return);
 			}
-			if (s[i] == '+')
-				number_Sign *= 1;
-			else if (s[i] == '-')
+			if (s[i] == '-')
 				number_Sign *= -1;
 			if (s[i] >= '0' && s[i] <= '9' && !bool_startOfNumber)
 			{
 				bool_startOfNumber = 1;
-				returned = s[i] - '0';
+				my_return = s[i] - '0';
 			}
 			else if (bool_startOfNumber)
-				returned = returned * 10 + (s[i] - '0');
-			i++;	
+				my_return = my_return * 10 + (s[i] - '0');
+			i++;
 		}
 	}
 	return (0);
