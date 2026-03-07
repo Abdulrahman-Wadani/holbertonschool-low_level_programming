aa3d19391e2786fa9096b398322751f1eeca29f7
Abdulrahman <dhomfff@gmail.com>
2026-03-07 07:19:41 +0300

    adding 6th task

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - Entry point
+ * @s: str
+ *
+ * Return: Always 0 (Success)
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] + 32;
+		i++;
+	}
+	return (s);
+}
