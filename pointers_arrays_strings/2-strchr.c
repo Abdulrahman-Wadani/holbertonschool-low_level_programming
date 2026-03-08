8ed2333d5d40123b5b27e19bfa73d9118673ab89
Abdulrahman <dhomfff@gmail.com>
2026-03-09 00:03:37 +0300

    add task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Entry point
+ * @s: str
+ * @c: char
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *p = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+		}
+		i++;
+	}
+
+	if (p == 0)
+		return (NULL);
+	return (p);
+
+}
