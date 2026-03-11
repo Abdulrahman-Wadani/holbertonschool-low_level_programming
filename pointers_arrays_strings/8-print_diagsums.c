51d114337ee3c6324c5134491046e8482c1f56af
Abdulrahman <dhomfff@gmail.com>
2026-03-11 18:08:24 +0300

    add task 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ * @a: arr
+ * @size: num
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j = 0, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[(i * size) + j];
+		j++;
+	}
+
+	j = size - 1;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[(i * size) + j];
+		j--;
+	}
+
+	printf("%d, %d", sum1, sum2);
+}
