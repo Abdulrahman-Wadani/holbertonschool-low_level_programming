5de6b2501f32536b14578e3d77fff71d7e513576
Abdulrahman <dhomfff@gmail.com>
2026-03-11 15:28:16 +0300

    add task 4

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: str
+ * @accept: str
+ *
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j = 0;
+	unsigned int r = 0;
+
+	while (accept[i])
+	{
+		while (s[j])
+		{
+			if (s[j] == accept[i])
+			{
+				r++;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (++r);
+}
