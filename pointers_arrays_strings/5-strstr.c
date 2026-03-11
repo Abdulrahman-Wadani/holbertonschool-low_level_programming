8dc5aa89f77610fa01a5637ce1e414e2eb669811
Abdulrahman <dhomfff@gmail.com>
2026-03-11 16:42:56 +0300

    add task 6

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - Entry point
+ * @haystack: str
+ * @needle: str
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, c = 0, d = 1;
+
+	while (needle[c])
+		c++;
+
+	while (haystack[i])
+	{
+		if (haystack[i] == needle[0])
+		{
+			while (d < c)
+				if (needle[d] == haystack[i + d])
+					d++;
+				else
+				{
+					d = 1;
+					break;
+				}
+			if (d == c)
+				return (&haystack[i]);
+
+		}
+		i++;
+	}
+
+	return (NULL);
+
+}
