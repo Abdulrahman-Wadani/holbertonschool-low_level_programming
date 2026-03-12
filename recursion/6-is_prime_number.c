e0e47025b46f479a39b541af7ab463675ef006ec
aw <dhomfff@gmail.com>
2026-03-13 01:27:50 +0300

    add task 0

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - short description
+ * @s: description
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
