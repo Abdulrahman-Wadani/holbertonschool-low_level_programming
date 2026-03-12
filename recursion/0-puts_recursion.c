ed2c838465945ebd901b13eddbffffc6993a88d2
aw <dhomfff@gmail.com>
2026-03-13 01:31:44 +0300

    add task 0 3

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -16,14 +16,3 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	_puts_recursion("Puts with recursion");
-	return (0);
-}
