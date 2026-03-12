b510596fc1ade370f7aea24690517a90eecb4ade
aw <dhomfff@gmail.com>
2026-03-13 02:03:14 +0300

    add task 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - short description
+ * @n: description
+ *
+ * Return: Nothing
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
