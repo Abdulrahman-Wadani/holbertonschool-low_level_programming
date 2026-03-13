f616d7026509cf00f4b632b1831eb1882edd3b8d
aw <dhomfff@gmail.com>
2026-03-13 08:26:53 +0300

    add task 4 2

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int i = 0;
+	int i = 1;
 	int total = 0;
 
 	if (argc < 2)
@@ -26,6 +26,7 @@ int main(int argc, char **argv)
 			printf("Error\n");
 			return (1);
 		}
+		i++;
 	}
 	printf("%d\n", total);
 	return (0);
