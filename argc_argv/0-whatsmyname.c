cc43e976f6ca95fc976d78d2824d7078543f9203
aw <dhomfff@gmail.com>
2026-03-13 07:08:20 +0300

    add task 0clear && echo 'Running Betty...' && echo 'Compiling...' && gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-whatsmyname.c -o 0-whatsmyname && echo 'Executing...' && ./0-whatsmyname && rm 0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * main - short description
+ * @argc: description
+ * @argv: s
+ *
+ * Return: Nothing
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
