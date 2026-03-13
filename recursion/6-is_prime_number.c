3b67c7ddc905b83131fcea07d57cb9e95c66e563
aw <dhomfff@gmail.com>
2026-03-13 03:24:30 +0300

    add task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - short description
+ * @n: description
+ *
+ * Return: Nothing
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (is_prime_number_sub(n, n - 1));
+}
+
+/**
+ * is_prime_number_sub - short description
+ * @n: description
+ * @j: description
+ *
+ * Return: Nothing
+ */
+int is_prime_number_sub(int n, int j)
+{
+	if (j == 1)
+		return (1);
+	if (n % j != 0)
+		return (is_prime_number_sub(n, j - 1));
+	return (0);
+
+}
