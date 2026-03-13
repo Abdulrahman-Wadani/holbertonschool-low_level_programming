4e77d03642a533d03f496ad4a69e8ed01d7f5c97
aw <dhomfff@gmail.com>
2026-03-13 07:55:30 +0300

    add task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,41 @@
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * main - short description
+ * @argc: description
+ * @argv: s
+ *
+ * Return: Nothing
+ */
+int main(int argc, char **argv)
+{
+	int i = 0, j = 0, c = 0;
+	int ch1 = 1, ch2 = 1;
+
+	if (argv[argc - 1][0] >= '0' && argv[argc - 2][0] <= '9')
+	{
+		while (argv[argc - 2][c])
+		{
+			if (argv[argc][c] == '-')
+				ch1 *= -1;
+			i *= 10;
+			i +=  argv[argc][c];
+			i *= ch1;
+			c++;
+		}
+		c = 0;
+		while (argv[argc - 1][c])
+		{
+			if (argv[argc][c] == '-')
+				ch2 *= -1;
+			j *= 10;
+			j +=  argv[argc][c];
+			j *= ch2;
+			c++;
+		}
+		printf("%d\n", (i * j));
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
