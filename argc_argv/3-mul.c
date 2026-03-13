2ea20b72a142877495286a4ac57a6a0bd82f0c5f
aw <dhomfff@gmail.com>
2026-03-13 07:59:48 +0300

    add task 3 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 			if (argv[argc - 2][c] == '-')
 				ch1 *= -1;
 			i *= 10;
-			i +=  argv[argc - 2][c];
+			i +=  argv[argc - 2][c] - '0';
 			i *= ch1;
 			c++;
 		}
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 			if (argv[argc - 1][c] == '-')
 				ch2 *= -1;
 			j *= 10;
-			j +=  argv[argc - 1][c];
+			j +=  argv[argc - 1][c] - '0';
 			j *= ch2;
 			c++;
 		}
