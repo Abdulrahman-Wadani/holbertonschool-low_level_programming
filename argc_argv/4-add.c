a8c0c737e21dbf7073dc36fd99f12e4555484d8e
aw <dhomfff@gmail.com>
2026-03-13 08:25:28 +0300

    add task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - short description
+ * @argc: description
+ * @argv: s
+ *
+ * Return: Nothing
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int total = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (argv[i])
+	{
+		if (atoi(argv[i]) && atoi(argv[i]) > 0)
+			total += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
