53967adc930eb39df10fa78e1acfaf0a3dd34371
Abdulrahman <dhomfff@gmail.com>
2026-03-29 17:37:03 +0300

    adding task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+/**
+ * create_array - description
+ * @size: description
+ * @c: char
+ *
+ * Return: description
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p = (char *) malloc(size);
+	unsigned int i;
+
+	if (size == 0 || p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
+}
