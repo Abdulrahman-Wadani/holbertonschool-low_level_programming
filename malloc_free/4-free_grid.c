09e038be3c4aa71b1d197052caa8df62cc9bc008
Abdulrahman <dhomfff@gmail.com>
2026-03-30 18:48:11 +0300

    adding task 4 2

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <stdlib.h>
+
 /**
  * free_grid - description
  * @grid: description
@@ -9,6 +12,8 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL || height <= 0)
+		return;
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
