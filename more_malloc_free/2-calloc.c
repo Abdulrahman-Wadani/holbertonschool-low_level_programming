1afac2f62bbb656f4faaa1e357d3c8dd4851014c
Abdulrahman <dhomfff@gmail.com>
2026-03-31 18:55:11 +0300

    adding task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - description
+ * @nmemb: description
+ * @size: int
+ *
+ * Return: description
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
