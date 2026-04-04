92ed169ed6a6100f6d68a3428059001e43ad7860
aw <dhomfff@gmail.com>
2026-04-04 17:58:33 +0300

    adding task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - description
+ * @d: description
+ * @name: description
+ * @age: description
+ * @owner: description
+ *
+ * Return: function that initialize a variable of type struct dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
