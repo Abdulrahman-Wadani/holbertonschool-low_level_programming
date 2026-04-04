7d73193a9494419f5b0c8591dbc840b7a4eb7a3e
aw <dhomfff@gmail.com>
2026-04-04 17:53:31 +0300

    adding task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog - description
+ * @name: description
+ * @age: description
+ * @owner: description
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* dog.h */
