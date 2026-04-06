df357780b947be0b9ac2f1ce456f7a4116f5e05f
Abdulrahman <dhomfff@gmail.com>
2026-04-06 18:50:43 +0300

    adding task 3 2

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	while (i < 5)
 	{
 		if (s[0] != NULL &&
-			ops[i].op[0] == s[0] && s[1] == NULL)
+			ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
