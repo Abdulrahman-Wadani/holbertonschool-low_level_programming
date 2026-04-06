a7cc6cd98b48e372408c1ffe426fd0316cd5c901
Abdulrahman <dhomfff@gmail.com>
2026-04-06 16:33:32 +0300

    adding task 0 1

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -11,7 +11,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || ((name[0] < 'A' && name[0] > 'Z') &&
-	 (name[0] < 'a' && name[0] > 'z')))
+	(name[0] < 'a' && name[0] > 'z')))
 		return;
 	f(name);
 }
