e90ffe32e0b2953056d68756361687c92ec11909
Abdulrahman <dhomfff@gmail.com>
2026-04-07 18:16:54 +0300

    adding task 1 3

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (i != n - 1 && separator != NULL)
-			printf("%d, ", va_arg(args, int));
+			printf("%d%s", va_arg(args, int), separator);
 		else
 			printf("%d", va_arg(args, int));
 	}
