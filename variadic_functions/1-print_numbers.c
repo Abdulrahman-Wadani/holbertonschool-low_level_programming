edbca2f8e0e3e9c725af4a8143787122a4869eb0
Abdulrahman <dhomfff@gmail.com>
2026-04-07 18:14:54 +0300

    adding task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - description
+ * @separator: description
+ * @n: description
+ *
+ * Return: description
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int val;
+
+	if (n == 0)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1 && separator != NULL)
+			printf("%d, ", va_arg(args, int));
+		else
+			printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+}
