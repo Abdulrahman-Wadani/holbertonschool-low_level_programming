4b2f42ca7d8515adca318c49d67595eecd718315
Abdulrahman <dhomfff@gmail.com>
2026-04-07 17:55:57 +0300

    adding task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "vaesintrin.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - description
+ * @n: description
+ *
+ * Return: description
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i;
+	int sum;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	sum = va_arg(args, int);
+
+	for (i = 1; i < n; i++)
+		sum += va_arg(args, int);
+
+	return (sum);
+}
