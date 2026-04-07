cb592f7c188041c5975fb7f9f7a4b58dc6d6a4b6
Abdulrahman <dhomfff@gmail.com>
2026-04-07 18:42:54 +0300

    adding task 2 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("nill%s", separator);
 		else if (s == NULL)
 			printf("nill");
-		else if (i != n - 1)
+		else if (i != n - 1 && separator != NULL)
 			printf("%s%s", s, separator);
 		else
 			printf("%s", s);
